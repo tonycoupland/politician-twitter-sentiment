3bb0cf6e52321ea0804ce40dc8a6a00216c236b2
tonycoupland <tony.coupland@gmail.com>
2017-06-06 10:07:52 +0100

    Changed controller

---
+++ PoliticianTwitterSentiment/Controllers/PoliticiansTweetsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoliticianTwitterSentiment.Controllers
+{
+    [Route("api/[controller]")]
+    public class PoliticiansTweetsController : Controller
+    {
+		// GET api/politicianstweets
+		[HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
